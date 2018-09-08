cbcb0a0aab24433f1950a1f074fa462d055b7d30
shankarsat <shankar.sathiamurthi@gmail.com>
2018-09-08 15:13:51 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,79 +1,190 @@
-
 (function ($) {
     "use strict";
-
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        return check;
+    $(".carousel-inner .item:first-child").addClass("active");
+    /* Mobile menu click then remove
+    ==========================*/
+    $(".mainmenu-area #mainmenu li a").on("click", function () {
+        $(".navbar-collapse").removeClass("in");
     });
 
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
+    // Select all links with hashes
+    $('.smoth a[href*="#"], .mainmenu-area .primary-menu a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+
+
+
+
+    /* Scroll to top
+    ===================*/
+    $.scrollUp({
+        scrollText: '<i class="fa fa-angle-up"></i>',
+        easingType: 'linear',
+        scrollSpeed: 900,
+        animation: 'fade'
+    });
+
+    /* testimonials Slider Active
+    =============================*/
+    $('.testimonials').owlCarousel({
+        loop: true,
+        margin: 0,
+        responsiveClass: true,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        smartSpeed: 1000,
+        navText: ['<i class="icofont icofont-rounded-left"></i>', '<i class="icofont icofont-rounded-right"></i>'],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+    // start easy pie chart plugin when skills section appear // 
+    $(".skills1").appear(function () {
+        $(".chart1").easyPieChart({
+            barColor: "#a4c8fd",
+            trackColor: "#e1e1e1",
+            scaleColor: false,
+            lineWidth: 3,
+            lineCap: "round",
+            size: 60,
+            animate: 1500
+        });
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+    }, {
+        accX: 0,
+        accY: -150
+    });
+
+    // start easy pie chart plugin when skills section appear // 
+    $(".skills2").appear(function () {
+        $(".chart2").easyPieChart({
+            barColor: "#800080",
+            trackColor: "#e1e1e1",
+            scaleColor: false,
+            lineWidth: 3,
+            lineCap: "round",
+            size: 60,
+            animate: 1500
+        });
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+    }, {
+        accX: 0,
+        accY: -150
+    });
+
+
+    $('.count').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+    // start mixitup plugin in portfolio section //
+    $("#filtering").mixItUp();
+
+
+    /*--------------------
+     MAGNIFIC POPUP JS
+     ----------------------*/
+    $('.video-bttn').magnificPopup({
+        type: 'iframe',
+        removalDelay: 500, //delay removal by X to allow out-animation
+        callbacks: {
+            beforeOpen: function () {
+                // just a hack that adds mfp-anim class to markup 
+                this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+                this.st.mainClass = this.st.el.attr('data-effect');
+            }
+        },
+        iframe: {
+            markup: '<div class="mfp-iframe-scaler">' +
+                '<div class="mfp-close"></div>' +
+                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                '<div class="mfp-title">Some caption</div>' +
+                '</div>'
+        },
+        gallery: {
+            enabled: true
+        },
+        closeOnContentClick: true,
+        midClick: true
+    });
+
+    /*------------------------------
+    Filter-PopUp
+	-------------------------------*/
+    $('.filter-popup').magnificPopup({
+        type: 'image',
+        removalDelay: 500, //delay removal by X to allow out-animation
+        callbacks: {
+            beforeOpen: function () {
+                // just a hack that adds mfp-anim class to markup 
+                this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+                this.st.mainClass = this.st.el.attr('data-effect');
+            }
+        },
+        gallery: {
+            enabled: true
+        },
+        closeOnContentClick: true,
+        midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    });
+
+    /* Preloader Js
+    ===================*/
+    $(window).on("load", function () {
+        $('.preloader').fadeOut(500);
+        /*WoW js Active
+        =================*/
+        new WOW().init({
+            mobile: false,
         });
     });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-
-    
-    
-    /*==================================================================
-    [ Simple slide100 ]*/
-
-    $('.simpleslide100').each(function(){
-        var delay = 7000;
-        var speed = 1000;
-        var itemSlide = $(this).find('.simpleslide100-item');
-        var nowSlide = 0;
-
-        $(itemSlide).hide();
-        $(itemSlide[nowSlide]).show();
-        nowSlide++;
-        if(nowSlide >= itemSlide.length) {nowSlide = 0;}
-
-        setInterval(function(){
-            $(itemSlide).fadeOut(speed);
-            $(itemSlide[nowSlide]).fadeIn(speed);
-            nowSlide++;
-            if(nowSlide >= itemSlide.length) {nowSlide = 0;}
-        },delay);
-    });
-
-
 })(jQuery);
